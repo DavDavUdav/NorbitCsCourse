2131d05cd26863929f83b636dc0e721c88bf9696
DavDavUdav <stepan05az@gmail.com>
2022-03-11 21:15:07 +0300

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ClassLibraryWrite
+{
+    public static class WriteStrings
+    {
+        /// <summary>
+        /// выводит квадрат из звездочек с пустым местом в центре.
+        /// </summary>
+        /// <param name="n"></param>
+        public static char[,] WriteStars(int n)
+        {
+
+            int result = (int)((n + 1) / 2);
+            char[,] starsSquade = new char[n, n];
+            for (int i = 1; i <= n; i++)
+            {
+
+                if (i == result)
+                {
+                    for (int j = 1; j < result; j++)
+                    {
+                        starsSquade[i - 1, j - 1] = '*';
+                    }
+                    starsSquade[result , result ] = ' ';
+                    for (int j = result+1; j <= n; j++)
+                    {
+                        starsSquade[i-1 , j-1 ] = '*';
+                    }
+                }
+                else
+                {
+                    for (int j = 1; j <= n; j++)
+                    {
+                        starsSquade[i - 1, j - 1] = '*';
+                    }
+                }
+
+            }
+            return starsSquade;
+        }
+
+        /// <summary>
+        /// выводит последовательность чисел.
+        /// </summary>
+        /// <param name="n"></param>
+        public static int[] WriteNum(int n)
+        {
+            int[] numbersArr = new int[n];
+            for (int i = 1; i <= n; i++)
+            {
+                numbersArr[i - 1] = i;
+            }
+            return numbersArr;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using ClassLibraryWrite;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadUser();
+        }
+
+        public static void ReadUser()
+        {
+            Console.WriteLine("Введите целое положительное число");
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int []arrNumbers = WriteStrings.WriteNum(n);
+            for (int i = 0; i<=arrNumbers.Length;i++)
+            {
+                if (i == n)
+                {
+                    Console.Write(i + ".");
+                }
+                else
+                {
+                    Console.Write(i + ", ");
+                }
+            }
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Введите целое нечетное положительное число");
+            n = Convert.ToInt32(Console.ReadLine());
+            char [,]starSquade = WriteStrings.WriteStars(n);
+
+            for (int i = 0; i<starSquade.GetLength(0);i++)
+            {
+                for (int j=0;j<starSquade.GetLength(1);j++)
+                {
+                    Console.Write(starSquade[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
