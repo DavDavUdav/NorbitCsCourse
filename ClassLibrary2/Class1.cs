c4753d905a59dd0bebdcd0f4bad3f5155fc1aa51
DavDavUdav <stepan05az@gmail.com>
2022-03-12 22:14:23 +0300

    создание консольного приложения и библиотеки для 2 дз

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassLibraryBmiWeight
+{
+    public class ClassBmiWeight
+    {
+        public static double Bmi()
+        {
+            int b = 1;
+
+            return b;
+        }
+    }
+}
